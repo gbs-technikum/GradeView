bc7eef32220795ead50ea93ed178b9542aef0c45
Leon Arnold <leon.arnold@t-online.de>
2018-01-25 08:43:02 +0100

    MenuItemClickListener added

--- app/src/main/java/gradieview/sabel/com/gradeview/MainActivity.java
+++ app/src/main/java/gradieview/sabel/com/gradeview/MainActivity.java
@@ -1,23 +1,39 @@
 package gradieview.sabel.com.gradeview;
 
+import android.content.ClipData;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+       private MenuItem menuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        menuItem = findViewById(R.id.faecherHinzufuegen);
+
+        menuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                Intent intent = new Intent(MainActivity.this, FaecherAuswahl.class);
+                startActivity(intent);
+                return true;
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return true;
+       MenuInflater inflater = getMenuInflater();
+       inflater.inflate(R.menu.menu, menu);
+       return true;
     }
+
+
 }
