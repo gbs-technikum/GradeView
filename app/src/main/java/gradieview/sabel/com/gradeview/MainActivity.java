6475ec872bbdc61fd8a894b43ae06f885b9099a5
Leon Arnold <leon.arnold@t-online.de>
2018-01-25 09:15:48 +0100

    vorhandener code formatiert

--- app/src/main/java/gradieview/sabel/com/gradeview/MainActivity.java
+++ app/src/main/java/gradieview/sabel/com/gradeview/MainActivity.java
@@ -10,38 +10,30 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-       private MenuItem menuItem;
+    private MenuItem menuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         menuItem = findViewById(R.id.faecherHinzufuegen);
-
-        //menuItem.getItemId();
-
-
-
-        }
-
-
-
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-       MenuInflater inflater = getMenuInflater();
-       inflater.inflate(R.menu.menu, menu);
-       return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.faecherHinzufuegen:
-                Intent intent = new Intent(this, FaecherAuswahl.class);
-                this.startActivity(intent);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
+            switch (item.getItemId()) {
+                case R.id.faecherHinzufuegen:
+                    Intent intent = new Intent(this, FaecherAuswahl.class);
+                    this.startActivity(intent);
+                    return true;
+            }
+            return super.onOptionsItemSelected(item);
     }
 }
