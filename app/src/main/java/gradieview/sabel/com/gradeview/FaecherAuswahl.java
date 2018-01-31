b9859e89899e3885b0db3dfcff6b5800324c79a3
Leon Arnold <leon.arnold@t-online.de>
2018-01-31 09:33:39 +0100

    FächerButtons hinzugefügt Fächer in Array gespeichert

--- app/src/main/java/gradieview/sabel/com/gradeview/FaecherAuswahl.java
+++ app/src/main/java/gradieview/sabel/com/gradeview/FaecherAuswahl.java
@@ -3,14 +3,47 @@ package gradieview.sabel.com.gradeview;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class FaecherAuswahl extends AppCompatActivity {
+
+    private Button btn_mathe;
+    private ArrayList<String> faecher;
+
+    public FaecherAuswahl() {
+        this.faecher = new ArrayList<>();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faecher_auswahl);
 
+        btn_mathe = findViewById(R.id.btn_mathe);
+        btn_mathe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                faecher.add(btn_mathe.getText().toString());
+                btn_mathe.setVisibility(View.GONE);
+                System.out.println(faecher);
+            }
+        });
+
         Intent intent = getIntent();
     }
+
+    public void speichern(){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putStringArrayListExtra("listeFaecher", faecher);
+
+    }
+
+
+
+
+
+
 }
--- app/src/main/java/gradieview/sabel/com/gradeview/MainActivity.java
+++ app/src/main/java/gradieview/sabel/com/gradeview/MainActivity.java
@@ -1,10 +1,8 @@
 package gradieview.sabel.com.gradeview;
 
-import android.content.ClipData;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -16,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        menuItem = findViewById(R.id.faecherHinzufuegen);
+        menuItem = findViewById(R.id.fachHinzufuegen);
     }
 
     @Override
@@ -29,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.faecherHinzufuegen:
+                case R.id.fachHinzufuegen:
                     Intent intent = new Intent(this, FaecherAuswahl.class);
                     this.startActivity(intent);
                     return true;
