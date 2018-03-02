cc4724b68962e02e9181d74ce3e4e8de2d115280
Leon Arnold <leon.arnold@t-online.de>
2018-03-02 12:39:02 +0100

    Fertig Button hinzugefügt

--- app/src/main/java/gradieview/sabel/com/gradeview/FachActivity.java
+++ app/src/main/java/gradieview/sabel/com/gradeview/FachActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -84,6 +86,14 @@ public class FachActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    // Menü hinzufügen in Leiste oben rechts
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menufaecher, menu);
+        return true;
     }
 
     public String getFachname() {
